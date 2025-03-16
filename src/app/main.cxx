cecd7a538a8bd5154fe9fff87ed8644e6933c81d
vasileios pashos <vapashos@gmail.com>
2025-03-16 20:10:39 +0200

    TaskScedulerCpp initial commit
    Contains a TaskSchedulerLib.lib and an executable TaskScheduleDriver that links to that shared library

---
+++ src/app/main.cxx
@@ -0,0 +1,98 @@
+﻿// TaskScheduleDriver.cpp : Defines the entry point for the application.
+//
+
+#include <TaskScheduler.h>
+#include <exception>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#ifdef _WIN32
+#include <Windows.h>
+#define LOAD_FUNC_ADDR(moduleHandler, funct)      GetProcAddress(moduleHandler, funct)
+#define MODULE_HANDLER                            HMODULE   
+#else
+#include <dlfcn.h>
+#define LOAD_FUNC_ADDR(moduleHandler, funct)      dlsym(moduleHandler, funct)
+#define MODULE_HANDLER                            void*
+#endif
+using namespace std;
+
+
+class LibraryLoader
+{
+public:
+
+    using CreateSchedulerFunc = ITaskScheduler* (*)(size_t,std::condition_variable&);
+
+
+    LibraryLoader(const char* libName)
+    {
+        #ifdef _WIN32
+        hModule_ = LoadLibrary((LPCSTR)libName);
+        #else
+        hModule_ = dlopen(libName,RTLD_LAZY);
+        #endif
+        if (!hModule_) {
+            throw exception("Failed to load DLL!\n");
+        }
+
+		cout << "DLL loaded successfully\n";
+    }
+    ~LibraryLoader()
+    {
+        #ifdef _WIN32
+        FreeLibrary(hModule_);
+        #else
+        dlclose(hModule_);
+        #endif
+    }
+
+    ITaskScheduler* CreateScheduler(size_t threads_nm,std::condition_variable& mainThreadCV)
+    {
+        if(!CreateScheduler_)
+        {
+            CreateScheduler_ = (CreateSchedulerFunc)LOAD_FUNC_ADDR(hModule_, "CreateScheduler");
+            if(!CreateScheduler_)
+                throw exception("Failed to load CreateScheduler function\n");
+        }
+
+        return CreateScheduler_(threads_nm,mainThreadCV);            
+    }
+
+private:
+    CreateSchedulerFunc CreateScheduler_    { nullptr };
+    MODULE_HANDLER      hModule_;
+
+};
+
+int main(int argc, char** argv)
+{
+    try {
+		LibraryLoader ll("TaskSchedulerLib.dll");
+        std::condition_variable cv_wait_for_workers;
+        ITaskSchedulerPtr scheduler(ll.CreateScheduler(20,cv_wait_for_workers));
+
+        const uint32_t num_of_tasks = 100;
+        
+        for(uint32_t i = 0; i < 100; i++)
+        {
+            scheduler->Schedule([]() { cout << "Task With Priority "<<endl; }, 1, 0.0);
+        }
+        
+        // Sleep for 2 seconds
+        // std::this_thread::sleep_for(std::chrono::seconds(2));
+        
+        std::mutex mtx_wait_for_workers;
+        
+        std::unique_lock<std::mutex> lock(mtx_wait_for_workers);
+        cv_wait_for_workers.wait(lock, [scheduler]() { return !scheduler->IsQueueEmpty(); });
+        
+		
+    }
+	catch (exception& e) {
+		cout << e.what();
+	}
+    return 0;
+}
---
+++ src/lib/TaskScheduler.h
@@ -0,0 +1,60 @@
+﻿// TaskSchedulerLib.h
+#pragma once
+#ifdef _WIN32
+#ifdef TASKSCHEDULERLIB_EXPORTS
+#define TASKSCHEDULERLIB_API __declspec(dllexport)
+#else
+#define TASKSCHEDULERLIB_API __declspec(dllimport)
+#endif
+#else
+#define TASKSCHEDULERLIB_API	
+#endif
+
+
+#include <cstdint>
+#include <memory>
+#include "TaskSchedulerTypes.h"
+
+using void_callback = void(*)();
+
+struct TASKSCHEDULERLIB_API LatencyStats
+{
+    double min{0.0};
+    double max{0.0};
+    
+    //! Returns the average latency
+    double Avg() const;
+};
+
+//! Class ITaskScheduler interface
+class TASKSCHEDULERLIB_API ITaskScheduler
+{
+public:
+    //! Creates an entity set in a pool        
+    //! \param threads number of threads of the thread pool managed by the scheduler
+    explicit ITaskScheduler(size_t threads);
+
+    //! Joins all running threads. Clean up of thread resources.
+    virtual ~ITaskScheduler();
+
+    //! Schedule a new task     
+    //! \param f void function pointer
+    //! \param priority of the task
+	//! \param deadline of the task
+    virtual void Schedule(void_callback f, uint32_t priority, double deadline) = 0;
+
+    //! Terminate the scheduler, release all resources
+    virtual void Terminate() = 0;
+
+    virtual bool IsQueueEmpty() const = 0;
+
+    //! Get the latency statistics of the scheduler
+    const LatencyStats* GetLatencyStats() const;
+
+
+protected:
+    size_t          threads_;
+    LatencyStats    lstats_;
+};
+
+using ITaskSchedulerPtr = std::shared_ptr<ITaskScheduler>;
